f4030c33034fe4b11f65dbd20ae743789739e491
Brian Han <brianhan87usa@gmail.com>
2015-06-07 13:50:54 -0500

    Add route middleware to adminRouter This will do something before each route in adminRouter gets requested

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.get('/', function(req, res) {
 
 var adminRouter = express.Router();
 
+// route middleware that will happen on every request.
+adminRouter.use(function(req, res, next) {
+  
+  // log each request to the console
+  console.log(req.method, req.url);
+
+  // continue doing what we were doing and go to the route
+  next();
+});
+
 adminRouter
   .get('/', function(req, res) {
     res.send('I am the dashboard');
