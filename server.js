5d7d1d11491f9bd20e4b11ef67b0e40bd95ebb46
Brian Han <brianhan87usa@gmail.com>
2015-06-07 13:46:58 -0500

    Create adminRouter instance

--- server.js
+++ server.js
@@ -6,5 +6,20 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+// create routes for the admin section
+// get an instance of the router
+
+var adminRouter = express.Router();
+
+adminRouter
+  .get('/', function(req, res) {
+    res.send('I am the dashboard');
+  })
+  .get('/users', function(req, res) {
+    res.send('I show all the users!');
+  });
+
+app.use('/admin', adminRouter);
+
 app.listen(1337);
 console.log('http://localhost:1337');
